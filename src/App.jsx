88359e0ab4440bc69f80c2e9107332cd90d027f9
arpit18-code <arpitjain18105@gmail.com>
2026-02-12 13:53:36 +0530

    Implemented lazy loading for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/home";
-import Important from "./Pages/important";
-import Bin from "./Pages/bin";
-import Archieve from "./Pages/archieve";
+import CircularProgress from "@mui/material/CircularProgress";
 function App() {
+  const Home = lazy(() => import("./Pages/home"));
+  const Important = lazy(() => import("./Pages/important"));
+  const Bin = lazy(() => import("./Pages/bin"));
+  const Archieve = lazy(() => import("./Pages/archieve"));
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Important" element={<Important />} />
-        <Route path="/Deleted" element={<Bin />} />
-        <Route path="/Archived" element={<Archieve />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center h-screen">
+            <CircularProgress color="primary" />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Important" element={<Important />} />
+          <Route path="/Deleted" element={<Bin />} />
+          <Route path="/Archived" element={<Archieve />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
